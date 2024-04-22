dfc70b941cb2d9a36707e19bea12e9d6c1778e60
Artur Almeida <artur.almeida@leafpayrh.com.br>
2024-04-22 20:58:44 -0300

    feat: components

--- src/Components/Comment/index.tsx
+++ src/Components/Comment/index.tsx
@@ -4,21 +4,21 @@ import { Flex, Text } from "@chakra-ui/react"
 
 type CommentProps = {
     text: string
-    photo: string
+    photo?: string
 }
 
-export default function Comment(){
+export default function Comment({photo, text}: CommentProps){
     return (
         <>
-            <Flex flexDir={"column"} boxShadow="5px 15px 30px 1px #cccccc" background={"#fff"} rounded={"10px"} minH="400px" minW="350px" >
-                <Text>asd</Text>
-                <Flex>
-                    <Text>Photo</Text>
+            <Flex flexDir={"column"} justifyContent={"space-around"} p="20px 20px 20px 40px" fontSize={14} boxShadow="5px 15px 30px 1px #cccccc" background={"#fff"} rounded={"10px"} minH="400px" maxW="350px" >
+                <Text w="90%" letterSpacing={"1px"} >{text}</Text>
+                <Flex alignItems={"center"}  gap="50px">
+                    <Text marginBlockStart={0} marginBlockEnd={0}>Photo</Text>
                     <Flex flexDir={"column"}>
-                        <Text>
+                        <Text marginBlockStart={0} marginBlockEnd={0}>
                             Name
                         </Text>
-                        <Text>
+                        <Text marginBlockStart={0} marginBlockEnd={0}>
                             Empresa
                         </Text>
                     </Flex>
